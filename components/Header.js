b3c560312162272e30526b3601ec4cd4fba80408
Tolkinxon <krosstony639@gmail.com>
2023-02-12 16:28:45 +0500

    first commit

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Header() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.textsArea}>
+        <h3>Model 3</h3>
+        <p>
+          Order Online
+          <span className={styles.touchless}>Touchless delivery</span>
+        </p>
+      </div>
+      <div className={styles.buttons}>
+        <div className={styles.btnMain}>
+          <span className={styles.btn}>custom order</span>
+          <span className={styles.btn}>exciting invertory</span>
+        </div>
+        <img
+          src="/images/down-arrow.svg"
+          alt="down"
+          className={styles.downIcon}
+        />
+      </div>
+    </section>
+  )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <img
+          src="/images/logo.svg "
+          alt="tesla logo"
+          loading="lazy"
+          className="teslaLogo"
+          width={130}
+        />
+      </Link>
+
+      <ul className={styles.navbar6}>
+        <li className={styles.items}>Model S</li>
+        <li className={styles.items}>Model 3</li>
+        <li className={styles.items}>Model X</li>
+        <li className={styles.items}>Model Y</li>
+        <li className={styles.items}>Solar Roof</li>
+        <li className={styles.items}>Solar Panels</li>
+      </ul>
+      <div className={styles.menu}>
+        <li className={styles.items}>Shop</li>
+        <li className={styles.items}>Account</li>
+        <li className={styles.items}>Menu</li>
+      </div>
+    </nav>
+  )
+}
