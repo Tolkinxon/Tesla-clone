aca6f7f921a9771d3554c186eae559947d4a85ac
Tolkinxon <krosstony639@gmail.com>
2023-02-22 23:42:58 +0500

    added clear fuction

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
+import { AuthUserProvider } from '@/context/AuthUserContext'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
 }
---
+++ pages/signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div><p>test</p></div>
+  )
+}
+
+export default signup
