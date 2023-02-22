0b41ca205b792195633a3956f431e2d784676a39
Tolkinxon <krosstony639@gmail.com>
2023-02-22 23:39:35 +0500

    added clear fuction

--- context/AuthUserContext.js
+++ context/AuthUserContext.js
@@ -9,4 +9,11 @@ const authUserContext = createContext({
   signOut: async () => {},
 })
 
-export function AuthUserProvider({ children }) {}
+export function AuthUserProvider({ children }) {
+  const auth = useFirebaseAuth()
+  return (
+    <authUserContext.Provider value={auth}>{children}</authUserContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(authUserContext)
