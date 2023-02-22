fc9597cf36389023514fca966064dbad1988a896
Tolkinxon <krosstony639@gmail.com>
2023-02-22 23:32:35 +0500

    added clear fuction

--- context/AuthUserContext.js
+++ context/AuthUserContext.js
@@ -1,2 +1,12 @@
 import { createContext, useContext } from 'react'
-import useFirebaseAuth  from '@/lib/useFirebaseAuth'
+import useFirebaseAuth from '@/lib/useFirebaseAuth'
+
+const authUserContext = createContext({
+  authUser: null,
+  loading: true,
+  signInWithEmailAndPassword: () => {},
+  createUserWithEmailAndPassword: () => {},
+  signOut: async () => {},
+})
+
+export function AuthUserProvider({ children }) {}
