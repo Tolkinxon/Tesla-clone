a6250449bff79861398c5cc97cb26e9d8464f17e
Tolkinxon <krosstony639@gmail.com>
2024-04-21 18:55:12 +0300

    all files cleaned

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import { AuthUserProvider } from '@/context/AuthUserContext'
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
-}
--- pages/signup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const signup = () => {
-  return (
-    <div><p>test</p></div>
-  )
-}
-
-export default signup
